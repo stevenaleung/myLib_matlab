987cdec2210f3e78236dce2ab029fc1a87bfbb5c
Steve Leung <stevealeung@gmail.com>
2019-04-04 16:34:51 -0700

    modified excludeFromLegend.m to take arrays of plot handles and also added -except functionality

--- excludeFromLegend.m
+++ excludeFromLegend.m
@@ -1,4 +1,18 @@
-function excludeFromLegend(h)
-% exclude the data associated with handle h from the legend entries
+function excludeFromLegend(h, varargin)
+% exclude plots from the legend entries
 
-set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+
+% default behavior is to exclude all plots in handles array
+inds = 1:length(h);
+
+% do not exclude handles that have exceptions
+if length(varargin)==2
+    if strfind(lower(varargin{1}), 'except')
+        inds(varargin{2}) = [];
+    end
+end
+
+% exclude the plots from the legend
+for i = inds
+    set(get(get(h(i),'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+end 
