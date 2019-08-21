151f1f53b66e678b130574ce779b0b941f1a8b99
Steve Leung <stevealeung@gmail.com>
2019-08-21 11:50:50 -0700

    renamed debugWorkspace.m to dbw.m

--- dbw.m
+++ dbw.m
@@ -1,4 +1,5 @@
-function debugWorkspace()
+function dbw()
+% dbw: short for debug workspace
 % function to open a blank workspace without clearing variables
 % return to the original workspace using dbquit
 
